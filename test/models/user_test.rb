e6117f9c7095bd020e779b862a151d5a0f614e66
Igor_Shum <Shumeikoigor@yandex.ru>
2020-04-08 19:05:07 +0300

    Добавлены тесты длины полей модели User

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,32 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  def setup
+		@user = User.new(email: "Ya@ya.ru", name: "Яндекс")  	
+  end
+
+  test "valid user?" do
+  	assert @user.valid?
+  end
+
+  test "name should be present" do
+  	@user.name = ""
+  	assert_not @user.valid?
+  end
+
+  test "email should be present" do
+  	@user.email = ""
+  	assert_not @user.valid?
+  end
+
+  test "name should not be too long" do 
+		@user.name = "a"*51
+		assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+  	@user.email = "b"*244+"example.com"
+  	assert_not @user.valid?
+  end
 end
