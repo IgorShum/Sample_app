f279f535a5e625fb182edf55d231d839fac93bab
Igor_Shum <Shumeikoigor@yandex.ru>
2020-09-22 11:53:04 +0300

    Name min 3

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,6 +50,13 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
   test "email should be unique" do
   	dublicate_user = @user.dup
   	@user.save
