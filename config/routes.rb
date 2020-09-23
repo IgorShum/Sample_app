8049b0ba94d45698d5f5035d490c78bb2096d696
Igor_Shum <Shumeikoigor@yandex.ru>
2020-09-23 20:30:24 +0300

    +Код для вставки картинки е-майла, граватар [Страница 252]

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-
+  
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get 'signup' => 'users#new'
+  resources :users
 end
