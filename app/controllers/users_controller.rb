0731037359b046b4c19c92672efd36ca20bde7d1
Igor_Shum <Shumeikoigor@yandex.ru>
2020-09-24 14:54:53 +0300

    +Добавлен тест валидных данных в форме регистрации

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   def create
   @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to(@user)
-      #render 'new'
     else
       render 'new'
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
   end
 
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      user_params = {
+          name:"TestName",
+          email: "TestMail@test.ts",
+          password: "testpass",
+          password_confirmation: "testpass"
+      }
+      post users_path, params: {user:user_params}
+      follow_redirect!
+    end
+    assert_template 'users/show'
+  end
+
 end
