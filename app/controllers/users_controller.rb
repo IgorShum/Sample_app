68a9d66ab2d168176cd718055f6de067abc0fb03
Igor_Shum <Shumeikoigor@yandex.ru>
2020-09-24 11:11:15 +0300

    Отображение User [Страница 271]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,20 +5,20 @@ class UsersController < ApplicationController
 
   def create
   @user = User.new(user_params)
-  if @user.save
-    render 'new'
-  else
-
+    if @user.save
+      redirect_to(@user)
+      #render 'new'
+    else
+      render 'new'
+    end
   end
-end
 
   def show
     @user = User.find(params[:id])
   end
 
-  private
-      def user_params
-        params.require(:user).permit(:name, :email, :password,
-                                     :password_confirmation)
-      end
+  def user_params
+
+    params.required(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
