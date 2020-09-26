f6a43bae1282ece7356d6ceabfdd6ffb92b7e9d0
Igor_Shum <Shumeikoigor@yandex.ru>
2020-09-26 14:07:29 +0300

    +Add routes [290]

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'login' => 'sessions#new'
+  post 'login' => 'session#create'
+  delete 'logout' => 'session#destroy'
   
   root 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
