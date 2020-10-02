c9087e9e98cb8e8c3591cb061b6537b67b1ee75e
Igor_Shum <Shumeikoigor@yandex.ru>
2020-10-02 03:55:01 +0300

    +Тесты Запрет редактирования для неавторизованных пользователей Проверка flash сообщений

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -8,17 +8,26 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   test "unsuccessful" do
     get edit_user_path @user
-    assert_template 'users/edit'
+
+    post login_path, params: { session: { email: @user.email, password: 'password', remember:'0' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
+    get edit_user_path @user
     patch user_path(@user), params: {user: { name:'',
                                      email: 'foo@invalid',
                                      password: 'foo',
                                      password_confirmation: 'bar'}}
     assert_template 'users/edit'
-    assert_not flash.empty?
-    assert_equal flash[:danger], 'Profile not updated!'
+    assert_equal flash[:danger], "Profile not updated!"
   end
 
   test "successful" do
+    get edit_user_path @user
+    post login_path, params: { session: { email: @user.email, password: 'password', remember:'0' } }
+    assert_redirected_to @user
+    follow_redirect!
+    assert_template 'users/show'
     get edit_user_path @user
     assert_template 'users/edit'
     name = "Foo Bar"
@@ -27,11 +36,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
                                              email: email,
                                              password: '',
                                              password_confirmation: ''}}
-    assert_not flash.empty?
-    assert_equal flash[:success], 'Profile updated!'
-    assert_redirected_to @user
+    #assert_redirected_to @user
+    puts "Before #{@user.inspect}"
     @user.reload
+    puts "After #{@user.inspect}"
     assert_equal @user.name, name
     assert_equal @user.email, email.downcase!
+    assert_equal flash[:success], "Profile updated!"
   end
 end
