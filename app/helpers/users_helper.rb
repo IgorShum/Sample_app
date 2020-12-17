9bcb4d0bd0a2fda3aaee61f02b1d266c5d93ba5c
IgorShum <Shumeikoigor@yandex.ru>
2020-12-17 03:22:28 +0300

    Хелпер скрытия е-майла

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,10 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: 'gravatar')
   end
+
+  def hide_email(email)
+    hidemail = email.chars
+    hidemail[2,3] = '*'
+    hidemail.join()
+  end
 end
