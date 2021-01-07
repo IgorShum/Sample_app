d5d49922331ec9a48f3563555323105f285ff21b
IgorShum <Shumeikoigor@yandex.ru>
2021-01-08 02:15:50 +0300

    +Ассоциации, +тесты

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   attr_accessor :remember_token
   before_save { email.downcase!}
+
+  has_many :microposts
+
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },
